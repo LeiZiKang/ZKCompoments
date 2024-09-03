0a97acc3f225484a88085f524b5804f28b042ae4
雷子康imback <leizikang@imbacktech.com>
2024-09-03 11:12:31 +0800

    添加extension部分代码，编写package.swift

--- Package.swift
+++ Package.swift
@@ -5,11 +5,17 @@ import PackageDescription
 
 let package = Package(
     name: "ZKCompoments",
+    platforms: [
+        .iOS(.v12)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ZKCompoments",
             targets: ["ZKCompoments"]),
+    ],
+    dependencies: [
+    // 在此添加依赖项
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
