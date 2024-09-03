9749a796aeb47ac9bf24b6a86d2b9dc505567de5
雷子康imback <leizikang@imbacktech.com>
2024-09-03 16:58:58 +0800

    实现ExampleApp

---
+++ ExampleApp/ExampleApp/ExampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleAppApp.swift
+//  ExampleApp
+//
+//  Created by MEETU on 3/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct ExampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
